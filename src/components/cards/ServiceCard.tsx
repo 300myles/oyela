9d077b9ab6b625dd3fcdf2b6508c910bf4762db2
Emeka-Ugwu Kamsy Myles <kamsyemekaugwu@gmail.com>
2024-10-22 18:41:52 +0100

    minor update

--- src/components/cards/ServiceCard.tsx
+++ src/components/cards/ServiceCard.tsx
@@ -1,22 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import MainButton from "../common/MainButton";
 
 interface IProps {
   iconUrl: string;
   title: string;
   description: string;
+  summary: string;
   action?: () => void;
 }
 
-function ServiceCard({ iconUrl, title, description, action }: IProps) {
+function ServiceCard({ iconUrl, title, description, summary, action }: IProps) {
+  const [viewMore, setViewMore] = useState(false);
+
   return (
     <div className="flex flex-1 md:min-w-[24rem] md:max-w-[24rem] flex-col gap-[2.56rem]  pt-[1.91rem] pb-[2.81rem] px-[1.5rem] items-center service-card-shadow rounded-[1.75rem]">
       <div>
         <img src={iconUrl} alt="service icon" />
       </div>
       <p className="text-[1.5rem] text-center font-[700]">{title}</p>
-      <p className="text-normal">{description}</p>
-      <MainButton text="Learn More" action={action} classes="w-[10.125rem]" />
+      <p className="text-normal">{viewMore ? description : summary}</p>
+      <MainButton text={viewMore ? "Show Less" : "Learn More"} action={() => setViewMore(!viewMore)} classes="w-[10.125rem] text-md" />
     </div>
   );
 }
--- src/components/sections/ServiceSection.tsx
+++ src/components/sections/ServiceSection.tsx
@@ -9,9 +9,8 @@ function ServiceSection() {
       id: 0,
       iconUrl: "/images/virtual-ai.png",
       title: `Virtual AI Counselor`,
-      description:
-        // "Our AI-powered Virtual Counselor provides instant, round-the-clock emotional support tailored to your needs. Whether you're feeling anxious, stressed, or need relationship advice, the Virtual Counselor is always available to listen and guide you. Using advanced emotional intelligence, it offers personalized responses, mindfulness exercises, and tips to help you navigate your emotions anytime, anywhere.",
-        "Our AI-powered Virtual Counselor provides instant, round-the-clock emotional support tailored to your needs. Whether you're feeling anxious, stressed, or need relationship advice, the Virtual Counselor is always available to listen and guide you.",
+      description: "Our AI-powered Virtual Counselor provides instant, round-the-clock emotional support tailored to your needs. Whether you're feeling anxious, stressed, or need relationship advice, the Virtual Counselor is always available to listen and guide you. Using advanced emotional intelligence, it offers personalized responses, mindfulness exercises, and tips to help you navigate your emotions anytime, anywhere.",
+      summary: "Our AI-powered Virtual Counselor provides instant, round-the-clock emotional support tailored to your needs. Whether you're feeling anxious, stressed, or need relationship advice, the Virtual Counselor is always available to listen and guide you.",
       features: [
         "Immediate emotional support.",
         "Personalized mindfulness and coping strategies.",
@@ -22,9 +21,8 @@ function ServiceSection() {
       id: 1,
       iconUrl: "/images/global.png",
       title: "Global Access to Professional Counselors",
-      description:
-        // "Connect with certified therapists and counselors from around the world through our app. We make it easy to schedule virtual sessions with professionals who understand your unique emotional needs. Whether it's relationship challenges, mental health concerns, or personal growth, you can find the right counselor for you and receive expert advice in a safe, confidential setting",
-        "Connect with certified therapists and counselors from around the world through our app. We make it easy to schedule virtual sessions with professionals who understand your unique emotional needs.",
+      description: "Connect with certified therapists and counselors from around the world through our app. We make it easy to schedule virtual sessions with professionals who understand your unique emotional needs. Whether it's relationship challenges, mental health concerns, or personal growth, you can find the right counselor for you and receive expert advice in a safe, confidential setting",
+      summary: "Connect with certified therapists and counselors from around the world through our app. We make it easy to schedule virtual sessions with professionals who understand your unique emotional needs.",
       features: [
         "Access to licensed counselors globally.",
         "Flexible scheduling for virtual therapy sessions.",
@@ -35,14 +33,13 @@ function ServiceSection() {
       id: 2,
       iconUrl: "/images/heart-care.png",
       title: "Therapy & Relationship Assessments",
-      description:
-        // "Our app offers in-depth personality and relationship assessments that help you understand yourself and your interactions better. These personalized assessments are designed by experts to offer insights into your emotional well-being, attachment styles, and relationship patterns. By understanding your strengths and areas for growth, you can make meaningful changes and enhance your emotional life.",
-        "Our app offers in-depth personality and relationship assessments that help you understand yourself and your interactions better. These personalized assessments are designed by experts to offer insights into your emotional well-being and relationships.",
-        features: [
-          "Tailored personality assessments.",
-          "Insights into your emotional well-being and relationships.",
-          "xpert advice for personal growth and stronger connections."
-        ],
+      description: "Our app offers in-depth personality and relationship assessments that help you understand yourself and your interactions better. These personalized assessments are designed by experts to offer insights into your emotional well-being, attachment styles, and relationship patterns. By understanding your strengths and areas for growth, you can make meaningful changes and enhance your emotional life.",
+      summary: "Our app offers in-depth personality and relationship assessments that help you understand yourself and your interactions better. These personalized assessments are designed by experts to offer insights into your emotional well-being and relationships.",
+      features: [
+        "Tailored personality assessments.",
+        "Insights into your emotional well-being and relationships.",
+        "xpert advice for personal growth and stronger connections."
+      ],
     },
   ];
   return (
@@ -54,6 +51,7 @@ function ServiceSection() {
             key={service.id}
             title={service.title}
             description={service.description}
+            summary={service.summary}
             iconUrl={service.iconUrl}
           />
         ))}
